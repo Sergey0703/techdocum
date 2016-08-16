7c17f93650371b03c676c8031bb3cde21e49ee20
Serg <sergey070373@gmail.com>
2016-08-16 17:04:30 +0300

    first orm3

--- src/AppBundle/Entity/Nomenclature.php
+++ src/AppBundle/Entity/Nomenclature.php
@@ -11,10 +11,10 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity
- * @ORM\Table(name="department")
+ * @ORM\Table(name="nomenclature")
  */
 
-class Department
+class Nomenclature
 {
     /**
      * @ORM\Column(type="integer")
