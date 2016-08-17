22cca77605c4351e320760714491bcb44fa35010
Serg <sergey070373@gmail.com>
2016-08-17 20:46:45 +0300

    orm5

---
+++ app/DoctrineMigrations/Version20160817183512.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20160817183512 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->addSql("CREATE TABLE department
+(id INT(11) AUTO_INCREMENT NOT NULL,
+name VARCHAR(100) NOT NULL,
+description VARCHAR(255) NOT NULL,
+PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE = InnoDB"
+        );
+
+    }
+
+
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+    }
+}
